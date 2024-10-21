fd45a0a644ea87392099c2d05801058e29939c0a
Adam Richard Washington Hazel <arhazel@student.uia.no>
2024-10-21 20:10:10 +0200

    Succesfully added radio button to register so that user can decide what type of user they are

--- Gruppeportalen_new/Data/ApplicationDbInitialiser.cs
+++ Gruppeportalen_new/Data/ApplicationDbInitialiser.cs
@@ -5,14 +5,27 @@ namespace Gruppeportalen_new.Data;
 
 public class ApplicationDbInitialiser
 {
-    public static void Initialize(ApplicationDbContext db, UserManager<ApplicationUser> um)
+    public static void Initialize(ApplicationDbContext db, UserManager<ApplicationUser> um, RoleManager<IdentityRole> rm)
     {
         db.Database.EnsureDeleted();
         db.Database.EnsureCreated();
         
+        // Create roles
+        var normalUser = new IdentityRole("NormalUser");
+        rm.CreateAsync(normalUser).Wait();
+        var centralOrg = new IdentityRole("centralOrg");
+        rm.CreateAsync(centralOrg).Wait();
+        
+        // Add users
         var user = new ApplicationUser {UserName = "user@uia.no", Email = "user@uia.no", EmailConfirmed = true };
         um.CreateAsync(user, "Password1.").Wait();
-
+        
+        var user2 = new ApplicationUser {UserName = "user2@uia.no", Email = "user2@uia.no", EmailConfirmed = true };
+        um.CreateAsync(user2, "Password1.").Wait();
+        
+        // Add user to role
+        um.AddToRoleAsync(user,"NormalUser").Wait();
+        
         db.SaveChanges();
     }
 }
--- Gruppeportalen_new/Models/ApplicationUser.cs
+++ Gruppeportalen_new/Models/ApplicationUser.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gruppeportalen_new.Models;
 
 public class ApplicationUser : IdentityUser
 {
-    
+    [Required]
+    [StringLength(50)]
+    public string TypeOfUser { get; set; } = string.Empty;
 }
